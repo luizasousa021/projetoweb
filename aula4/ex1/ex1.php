0a36d8520d79d9b254bb662aa85f4863e44e0c80
luizasousa021 <112010203+luizasousa021@users.noreply.github.com>
2022-10-06 16:59:08 -0300

    Add files via upload

---
+++ aula4/ex1/ex1.php
@@ -0,0 +1,7 @@
+<?php
+
+$idade = $_GET["idade"];
+if($idade >= 16){
+    echo"Pode Voltar...";
+}
+
---
+++ aula4/ex1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="ex1.php" method="get">
+        Qual é a sua idada : <br/>
+        <input type="number" name="idade"/>
+        <br/><br/>
+        <input type="sumbit" value="verificar"/>
+    </form>
+    
+</body>
+</html>
